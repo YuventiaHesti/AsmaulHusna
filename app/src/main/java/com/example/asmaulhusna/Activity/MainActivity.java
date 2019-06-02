f83152eb86ba193fcf735ba6500ec5510ed22186
Yandi Mulyadi <narutositez99@gmail.com>
2019-06-02 17:51:48 +0700

    2 Juni 2019

--- app/src/main/java/com/example/asmaulhusna/Activity/MainActivity.java
+++ app/src/main/java/com/example/asmaulhusna/Activity/MainActivity.java
@@ -1,31 +1,39 @@
 package com.example.asmaulhusna.Activity;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.asmaulhusna.R;
 
 public class MainActivity extends AppCompatActivity {
-    Button clickme;
-
+    TextView tvSplash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //menghilangkan ActionBar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-        clickme=(Button)findViewById(R.id.btnMenuUtama);
+        tvSplash = (TextView) findViewById(R.id.tvSplash);
 
-        clickme.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
                 changeActivity();
+                finish();
             }
-        });
+        }, 3000); //3000 L = 3 detik
     }
+
     private void changeActivity(){
         Intent intent = new Intent(this,MenuUtama.class);
         startActivity(intent);
